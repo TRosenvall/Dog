f37b963447ffe3535b07a39494d6636eb7a41480
Timothy Rosenvall <timothy.rosenvall@gmail.com>
2019-07-03 14:21:30 -0600

    first commit

---
+++ IThinkYouDroppedYourUpDog/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  IThinkYouDroppedYourUpDog
+//
+//  Created by Timothy Rosenvall on 7/3/19.
+//  Copyright © 2019 Timothy Rosenvall. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
